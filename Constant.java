c2cede01d7ff6bb73c61d7f05420717bc5a329e7
Archan Roychoudhury <45584024+Kuekuatsheu95@users.noreply.github.com>
2019-01-09 09:51:31 +0530

    Add files via upload

---
+++ Constant.java
@@ -0,0 +1,21 @@
+ /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FitnessFunction;
+
+/**
+ *
+ * @author HP
+ */
+public class Constant {
+    
+
+public static final int NO_OF_TASKS = 100 ; // number of Cloudlets;
+
+    public static  int NO_OF_DATA_CENTERS =10; // number of Datacenters;
+
+    public static final int POPULATION_SIZE = 300; // Number of Particles.
+
+}
---
+++ SchedulerParticleUpdate.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FitnessFunction;
+
+/**
+ *
+ * @author HP
+ */
+import net.sourceforge.jswarm_pso.Particle;
+
+import net.sourceforge.jswarm_pso.ParticleUpdate;
+
+import net.sourceforge.jswarm_pso.Swarm;
+
+
+
+public class SchedulerParticleUpdate extends ParticleUpdate {
+
+    private static final double W = 0.9;
+
+    private static final double C = 2.0;
+
+
+
+    SchedulerParticleUpdate(Particle particle) {
+
+        super(particle);
+
+    }
+
+
+
+    @Override
+
+    public void update(Swarm swarm, Particle particle) {
+
+        double[] v = particle.getVelocity();
+
+        double[] x = particle.getPosition();
+
+        double[] pbest = particle.getBestPosition();
+
+        double[] gbest = swarm.getBestPosition();
+
+
+
+        for (int i = 0; i < Constant.NO_OF_TASKS; ++i) {
+
+            v[i] = W * v[i] + C * Math.random() * (pbest[i] - x[i]) + C * Math.random() * (gbest[i] - x[i]);
+
+            x[i] = (int) (x[i] + v[i]);
+
+        }
+
+    }
+
+}
